d20f02552c763e3af33ddf1d8d1b16d8fb480828
ZhenianaKozhan <94985681+ZhenianaKozhan@users.noreply.github.com>
2023-05-09 21:30:39 +0300

    good job)

--- src/components/Placeholder/Placeholder.jsx
+++ src/components/Placeholder/Placeholder.jsx
@@ -1,12 +1,12 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 const Placeholder = () => {
   return (
-    <div className="placeholder">
+    <div style={{ padding: '100px', textAlign: 'center', color: 'purple' }}>
       <h1>Hello</h1>
       <p>
         It is a test project, created by React. You can evaluate the work of the
-        project by clicking on the menu Tweets or{" "}
+        project by clicking on the menu Tweets or{' '}
         <Link to="/tweets">"here"</Link>.
       </p>
     </div>
